d86d1e328a11acb563f1e73ae821b4b8003eb437
zhangenzhi <zhangsuiyu657@gmail.com>
2021-10-26 14:16:17 +0800

    test: fuse models.

--- main.py
+++ main.py
@@ -22,22 +22,22 @@ if __name__ == '__main__':
     trainer.model.summary()
     trainer.uniform_self_evaluate()
 
-    # plotter_args = {'num_evaluate': 100,
-    #                 'step': 1/1000,
-    #                 'fuse_models': trainer_args['model']['fuse_models'],
-    #                 }
-    # plotter = Plotter(plotter_args, trainer.model)
+    plotter_args = {'num_evaluate': 100,
+                    'step': 1/1000,
+                    'fuse_models': trainer_args['model']['fuse_models'],
+                    }
+    plotter = Plotter(plotter_args, trainer.model)
 
-    # # set init state
-    # normalized_random_direction = plotter.create_random_direction(norm='layer')
-    # plotter.set_weights([normalized_random_direction], init_state=True)
+    # set init state
+    normalized_random_direction = plotter.create_random_direction(norm='layer')
+    plotter.set_weights([normalized_random_direction], init_state=True)
 
-    # # plot num_evaluate*fuse_models points in lossland
-    # start_time = time.time()
-    # for i in range(plotter.num_evaluate):
-    #     plotter.set_weights([normalized_random_direction])
-    #     avg_loss = trainer.uniform_self_evaluate()
-    #     with open("sp_result_10000.csv", "ab") as f:
-    #         np.savetxt(f, avg_loss, comments="")
-    # end_time = time.time()
-    # print("total time {}".format(end_time-start_time))
+    # plot num_evaluate*fuse_models points in lossland
+    start_time = time.time()
+    for i in range(plotter.num_evaluate):
+        plotter.set_weights([normalized_random_direction])
+        avg_loss = trainer.uniform_self_evaluate()
+        with open("sp_result_10000.csv", "ab") as f:
+            np.savetxt(f, avg_loss, comments="")
+    end_time = time.time()
+    print("total time {}".format(end_time-start_time))
