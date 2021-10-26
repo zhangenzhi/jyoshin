6e0ec829013980d1cbb137a508b7cc1f282b09d4
zhangenzhi <zhangsuiyu657@gmail.com>
2021-10-26 12:06:53 +0800

    test: fuse models.

--- main.py
+++ main.py
@@ -28,13 +28,12 @@ if __name__ == '__main__':
     plotter = Plotter(plotter_args, trainer.model)
 
     # # # set init state
-    # normalized_random_direction = plotter.create_random_direction(norm='layer')
-    # plotter.set_weights([normalized_random_direction])
+    normalized_random_direction = plotter.create_random_direction(norm='layer')
 
     # # # plot N points in lossland
     start_time = time.time()
     for i in range(plotter.num_evaluate):
-        # plotter.set_weights([normalized_random_direction])
+        plotter.set_weights([normalized_random_direction])
         avg_loss = trainer.uniform_self_evaluate()
     #     with open("result_10000.csv", "ab") as f:
     #         np.savetxt(f, [avg_loss], comments="")
