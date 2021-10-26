188d0933c81aa1f931cc0129af283fd386a95e44
zhangenzhi <zhangsuiyu657@gmail.com>
2021-10-26 23:38:54 +0800

    test: fuse models.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                     'optimizer': {'name': 'SGD', 'learning_rate': 0.001},
                     'dataset': {'name': 'uniform', 'batch_size': 100, 'epoch': 1},
                     'model': {'name': 'DNN', 'units': [64, 16, 1],
-                              'activations': ['tanh', 'tanh', 'tanh'], 'fuse_models': 10},
+                              'activations': ['tanh', 'tanh', 'tanh'], 'fuse_models': 1000},
                     }
 
     trainer = Trainer(trainer_args)
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     trainer.model.summary()
     # trainer.uniform_self_evaluate()
 
-    plotter_args = {'num_evaluate': 100,
-                    'step': 1/1000,
+    plotter_args = {'num_evaluate': 10000,
+                    'step': 1/100000,
                     'fuse_models': trainer_args['model']['fuse_models'],
                     }
     plotter = Plotter(plotter_args, trainer.model)
