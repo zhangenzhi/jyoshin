fec9cd143e258d113169fb5d1419100d2e23a28c
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-23 20:39:40 +0800

    cifar10 metric.

--- trainer/cifar10_trainer.py
+++ trainer/cifar10_trainer.py
@@ -45,7 +45,7 @@ class Cifar10Trainer(BaseTrainer):
             zip(grad, self.model.trainable_variables))
 
         # metric update
-        self.metric.update_state(loss)
+        self.metric.update_state(prediction,labels)
         return loss
 
     def run(self):
