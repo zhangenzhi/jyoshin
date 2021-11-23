d0911f2d874f39be26f2218d4333146be405f7fc
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-23 20:42:12 +0800

    cifar10 metric.

--- trainer/cifar10_trainer.py
+++ trainer/cifar10_trainer.py
@@ -52,6 +52,7 @@ class Cifar10Trainer(BaseTrainer):
         
         iter_ds = iter(self.dataset)
         start_time = time.time()
+        flag=0
         while True:
             try:
                 x = iter_ds.get_next()
@@ -59,8 +60,10 @@ class Cifar10Trainer(BaseTrainer):
                 print_warning("run out of dataset.")
                 break
             loss = self.train_step(x)
-            print("loss:{}, metric:{}".format(loss.numpy(), self.metric.result().numpy()))
-            self.metric.reset_states()
+            if flag %500 ==0:
+                print("loss:{}, metric:{}".format(loss.numpy(), self.metric.result().numpy()))
+        self.metric.reset_states()
+        flag +=1
             
         end_time = time.time()
         print("training cost:{}".format(end_time - start_time))
