477d79cceb54fab1bcdfbafd04f2166a067291b3
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-30 13:55:23 +0800

    2d plotter for cifar10 dnn.

--- label_generator.py
+++ label_generator.py
@@ -51,8 +51,6 @@ def generate_label_for_cifar10(model, dataset, path_to_file='./', filename='labe
         print(flag)
         y = model(x['x'])
         y = tf.squeeze(y)
-        # import pdb
-        # pdb.set_trace()
         y = tf.argmax(y, -1)
         if isinstance(labeled_data, type(None)):
             labeled_data = y
