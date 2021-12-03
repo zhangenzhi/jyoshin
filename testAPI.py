263cb794bf5ad8f713604dbf59f6427744e70475
zhangenzhi <zhangsuiyu657@gmail.com>
2021-12-03 16:24:25 +0800

    test: test load whole data to gpu.

--- testAPI.py
+++ testAPI.py
@@ -2,32 +2,32 @@ import pdb
 import tensorflow as tf
 import h5py
 import os
-os.environ["CUDA_VISIABLE_DEVICE"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 physical_devices = tf.config.list_physical_devices('GPU')
 for item in physical_devices:
     tf.config.experimental.set_memory_growth(item, True)
 
-i = tf.constant(0)
-c = lambda i: tf.less(i, 2**20)
-b = lambda i: (tf.add(i, 1), )
-r = tf.while_loop(c, b, [i])
+# i = tf.constant(0)
+# c = lambda i: tf.less(i, 2**20)
+# b = lambda i: (tf.add(i, 1), )
+# r = tf.while_loop(c, b, [i])
 
-# with tf.device("/device:gpu:0"):
-#     # data = tf.zeros(shape=[50000, 32, 32, 3])
-#     slice_y = tf.zeros(shape=[32*32*3, 1])
-#     slice_data = tf.zeros(shape=[500, 32, 32, 3])
-#     slice_data = tf.reshape(slice_data, shape=(500, -1))
+with tf.device("/device:gpu:0"):
+    # data = tf.zeros(shape=[50000, 32, 32, 3])
+    slice_y = tf.zeros(shape=[32*32*3, 1])
+    slice_data = tf.zeros(shape=[500, 32, 32, 3])
+    slice_data = tf.reshape(slice_data, shape=(500, -1))
 
-#     # while True:
-#     #     # for i in range(100):
-#     #     output = tf.matmul(slice_data, slice_y)
-#     x = slice_data
-#     y = slice_y
-#     i = tf.constant(0)
-#     def f(i, x, y): return tf.matmul(x, y)
-#     def c(i, x, y): return tf.less(i, 2 ** 20)
-#     r = tf.while_loop(cond=c, body=f, loop_vars=(i, x, y))
+    # while True:
+    #     # for i in range(100):
+    #     output = tf.matmul(slice_data, slice_y)
+    x = slice_data
+    y = slice_y
+    i = tf.constant(0)
+    def f(i, x, y): return tf.matmul(x, y)
+    def c(i, x, y): return tf.less(i, 2 ** 20)
+    r = tf.while_loop(cond=c, body=f, loop_vars=(i, x, y))
 
 # f1 = h5py.File("./saved_models/1/model.h5")
 # f2 = h5py.File("./saved_models/2/model.h5")
