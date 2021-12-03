088429ee840e457c36ff4e0c4d08b282ad35e40e
zhangenzhi <zhangsuiyu657@gmail.com>
2021-12-03 17:53:30 +0800

    test: test load whole data to gpu.

--- testAPI.py
+++ testAPI.py
@@ -13,6 +13,7 @@ physical_devices = tf.config.list_physical_devices('GPU')
 for item in physical_devices:
     tf.config.experimental.set_memory_growth(item, True)
 
+
 def on_device_matmul():
     with tf.device("/device:gpu:0"):
         slice_y = tf.zeros(shape=[32*32*3, 1])
@@ -24,9 +25,9 @@ def on_device_matmul():
         #     output = tf.matmul(slice_data, slice_y)
 
         # whille loop: total time: 49.9278666973114
-        # 8-body:  total time: 32.072572231292725
-        # 16-body: total time: 30.435903072357178
-        # 32-body: total time: 28.816709756851196
+        # 8-bodys:  total time: 32.072572231292725
+        # 16-bodys: total time: 30.435903072357178
+        # 32-bodys: total time: 28.816709756851196
         # i = tf.constant(0)
         # while tf.less(i, 2**20):
         #     output = tf.matmul(slice_data, slice_y)
@@ -37,9 +38,9 @@ def on_device_matmul():
             return tf.less(i, 2**20)
 
         def f(i, x, y):
-            output = tf.matmul(x, y)
-            return tf.add(i,1), x, y
-        
+            tf.matmul(x, y)
+            return tf.add(i, 1), x, y
+
         r = tf.while_loop(cond=c, body=f, loop_vars=(0, slice_data, slice_y))
 
 
