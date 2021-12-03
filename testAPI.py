7d3f9eb4538773e528f3b3f74b4fd44100fb0db4
zhangenzhi <zhangsuiyu657@gmail.com>
2021-12-03 16:27:54 +0800

    test: test load whole data to gpu.

--- testAPI.py
+++ testAPI.py
@@ -1,8 +1,10 @@
 import pdb
-import tensorflow as tf
 import h5py
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
 
 physical_devices = tf.config.list_physical_devices('GPU')
 for item in physical_devices:
@@ -13,24 +15,20 @@ for item in physical_devices:
 # b = lambda i: (tf.add(i, 1), )
 # r = tf.while_loop(c, b, [i])
 
-with tf.device("/device:gpu:0"):
-    # data = tf.zeros(shape=[50000, 32, 32, 3])
-    slice_y = tf.zeros(shape=[32*32*3, 1])
-    slice_data = tf.zeros(shape=[500, 32, 32, 3])
-    slice_data = tf.reshape(slice_data, shape=(500, -1))
+@tf.function
+def on_device_matmul():
+    with tf.device("/device:gpu:0"):
+        # data = tf.zeros(shape=[50000, 32, 32, 3])
+        slice_y = tf.zeros(shape=[32*32*3, 1])
+        slice_data = tf.zeros(shape=[500, 32, 32, 3])
+        slice_data = tf.reshape(slice_data, shape=(500, -1))
 
-    while True:
-        # for i in range(100):
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
-        output = tf.matmul(slice_data, slice_y)
+        while True:
+            # for i in range(100):
+            output = tf.matmul(slice_data, slice_y)
+            
+if __name__ == '__main__':
+    on_device_matmul()
         
     # x = slice_data
     # y = slice_y
