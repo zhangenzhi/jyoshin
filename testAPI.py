0859e7ffbb1fd01a51ee360af816faca1edbf9b0
zhangenzhi <zhangsuiyu657@gmail.com>
2021-12-03 18:02:21 +0800

    test: test load whole data to gpu.

--- testAPI.py
+++ testAPI.py
@@ -13,7 +13,7 @@ physical_devices = tf.config.list_physical_devices('GPU')
 for item in physical_devices:
     tf.config.experimental.set_memory_growth(item, True)
 
-
+@tf.function(experimental_relax_shapes=True)
 def on_device_matmul():
     with tf.device("/device:gpu:0"):
         # x = tf.zeros(shape=[500, 32*32*3])
