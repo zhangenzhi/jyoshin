d3840e6fa17d33653470ac44b5eafe0b5a19ee81
zhangenzhi <zhangsuiyu657@gmail.com>
2021-12-03 17:02:19 +0800

    test: test load whole data to gpu.

--- testAPI.py
+++ testAPI.py
@@ -24,10 +24,28 @@ def on_device_matmul():
         #     output = tf.matmul(slice_data, slice_y)
             
         # whille-loop: total time: 49.9278666973114
-        # 8-body: total time: 32.072572231292725
+        # 8-body:  total time: 32.072572231292725
+        # 16-body: total time: 30.435903072357178
+        # 32-body: total time: 30.435903072357178
         # tf.function: total time: 82.62994313240051
         i = tf.constant(0)
-        while tf.less(i, 2**16):
+        while tf.less(i, 2**15):
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
+            output = tf.matmul(slice_data, slice_y)
             output = tf.matmul(slice_data, slice_y)
             output = tf.matmul(slice_data, slice_y)
             output = tf.matmul(slice_data, slice_y)
