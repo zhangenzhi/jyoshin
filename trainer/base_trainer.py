85bcd3cbf3b34773e5f4169067848015fe92dd00
zhangenzhi <zhangsuiyu657@gmail.com>
2022-01-06 12:57:18 +0800

    test: trail weights save.

--- trainer/base_trainer.py
+++ trainer/base_trainer.py
@@ -96,7 +96,7 @@ class BaseTrainer:
     def save_weights_trajectory(self, index, filepath, filename='trail.h5'):
         check_mkdir(filepath)
         save_to_hdf5 = os.path.join(filepath, filename)
-        with h5py.File(save_to_hdf5, "w") as f:
+        with h5py.File(save_to_hdf5, "a") as f:
             grp = f.create_group("weights_{}".format(str(index)))
             for i, w in enumerate(self.model.weights):
                 grp.create_dataset(str(i), data=w.numpy())
